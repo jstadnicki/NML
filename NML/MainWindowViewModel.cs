6f10baf4af3913d34b82b2eff88a28c95a16ef81
jstadnicki <jaroslaw.stadnicki@gmail.com>
2013-11-15 22:08:09 +0100

    Viewmodel updated

--- NML/MainWindowViewModel.cs
+++ NML/MainWindowViewModel.cs
@@ -1,7 +1,47 @@
 ﻿namespace NML
 {
-    public class MainWindowViewModel
-    {
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
 
+    using NML.Annotations;
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string queryText;
+
+        public string QueryText
+        {
+            get
+            {
+                return this.queryText;
+            }
+            set
+            {
+                this.ShouldTriggerSearch(value);
+                this.queryText = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private void ShouldTriggerSearch(string query)
+        {
+            if (query.Length > 3)
+            {
+                MessageBox.Show("go");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
