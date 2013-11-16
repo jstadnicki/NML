49c8db613c1a3df2fad0179e798debfd3343879a
wojciechsura <wojciechsura@gmail.com>
2013-11-15 19:04:47 -0800

    Flat button for configuration

---
+++ NML/Controls/FlatButton.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NML.Controls
+{
+    /// <summary>
+    /// Interaction logic for FlatButton.xaml
+    /// </summary>
+    public partial class FlatButton : Button
+    {
+        public FlatButton()
+        {
+            InitializeComponent();
+        }
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Image.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(ImageSource), typeof(FlatButton), new PropertyMetadata(null));        
+    }
+}
