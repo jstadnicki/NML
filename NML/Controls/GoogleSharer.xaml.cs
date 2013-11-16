ef84e655afc6ca10baa8678ca0fad8bdae7cf778
pgs-pkrzywniak <pkrzywniak@pgs-soft.com>
2013-11-15 20:47:18 -0800

    G+

---
+++ NML/Controls/GoogleSharer.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NML.Controls
+{
+    public partial class GoogleSharer : UserControl
+    {
+        public GoogleSharer()
+        {
+            InitializeComponent();
+        }
+
+        private void GoogleSharer_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var p = new Process();
+            p.StartInfo.FileName = string.Format("https://plus.google.com/share?url={0}", HttpUtility.UrlEncode(ShareUrl));
+            p.Start();
+        }
+
+        public string ShareUrl
+        {
+            get { return (string)GetValue(ShareUrlProperty); }
+            set { SetValue(ShareUrlProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ShareUrl.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ShareUrlProperty =
+            DependencyProperty.Register("ShareUrl", typeof(string), typeof(GoogleSharer), new PropertyMetadata(string.Empty));
+
+        
+    }
+}
