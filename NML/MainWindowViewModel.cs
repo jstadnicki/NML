9962c3b7e9cc69c924321ff4f8ba86320fdc42e2
andy250 <akowal@outlook.com>
2013-11-15 16:47:58 -0800

    FIX: restored filtered engines

--- NML/MainWindowViewModel.cs
+++ NML/MainWindowViewModel.cs
@@ -1,4 +1,8 @@
-﻿namespace NML
+﻿using System;
+using System.Linq;
+using NML.Core;
+
+namespace NML
 {
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
@@ -105,7 +109,9 @@
 
             List<Task> tasks = new List<Task>();
 
-            foreach (var engine in this.engines)
+            var filteredEngines = GetFilteredEngines(this.QueryText);
+
+            foreach (var engine in filteredEngines)
             {
                 var queryTask = Task.Factory.StartNew(() => { return engine.Search(this.QueryText); });
 
@@ -170,6 +176,19 @@
             {
                 handler(this, new PropertyChangedEventArgs(propertyName));
             }
+        }
+
+        private IEnumerable<ISearchEngine> GetFilteredEngines(string query)
+        {
+            var prefixIndex = query.IndexOf(Constants.QueryPrefixSeparator);
+
+            if (prefixIndex > 0)
+            {
+                var queryPrefix = query.Substring(0, prefixIndex);
+                return engines.Where(e => e.Prefix.Equals(queryPrefix, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+            return this.engines;
         }
     }
 }
